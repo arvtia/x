25a71e452d080af5bd63e932ebf4099d53346163
Arvtia <arvazanakuzhy@gmail.com>
2025-09-19 20:33:04 +0530

    making the website progress work applicaton

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { registerSW } from './swRegistration.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+registerSW()
